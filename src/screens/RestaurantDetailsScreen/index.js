6196070181cbf781875f2c6d74372f33cf0bfabe
ricardo <ricky032929@gmail.com>
2022-08-18 19:31:25 -0400

    created homeScreen

---
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -0,0 +1,15 @@
+import { View, Text, Image, FlatList } from "react-native"
+import { StyleSheet } from "react-native"
+const RestaurantDetailsScreen = () =>{
+    return(
+        <View>
+            <Text>RestaurantDetailsPage</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default RestaurantDetailsPage
