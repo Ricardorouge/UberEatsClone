af363aeebdc48d3bdeeb5f0f77168ca5455b7031
ricardo <ricky032929@gmail.com>
2022-08-19 00:50:24 -0400

    added bottom navbar

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Foundation, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/HomeScreen";
 import RestaurantDetailsScreen from "../screens/RestaurantDetailsScreen";
@@ -12,7 +14,7 @@ const Stack = createNativeStackNavigator();
 const RootNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeTabs} />
       <Stack.Screen
         name="Restaurant"
         component={RestaurantDetailsScreen}
@@ -23,6 +25,45 @@ const RootNavigator = () => {
       <Stack.Screen name="OrderScreen" component={OrderScreen} />
       <Stack.Screen name="OrderDetails" component={OrderDetails} />
     </Stack.Navigator>
+  );
+};
+const Tab = createMaterialBottomTabNavigator();
+
+const HomeTabs = () => {
+  return (
+    <Tab.Navigator
+      barStyle={{
+        backgroundColor:'white',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Foundation name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Orders"
+        component={OrderScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="list-alt" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user-alt" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
